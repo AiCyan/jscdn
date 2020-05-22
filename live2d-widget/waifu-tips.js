de750f9043c42ce15c461fbf66e2c529fac5e620
aicyan-iu <aicyan@outlook.com>
2020-05-22 18:07:28 +0800

    修改live2d/css/js/json

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -18,14 +18,14 @@ function loadWidget(config) {
 			<canvas id="live2d" width="800" height="800"></canvas>
 			<div id="waifu-tool">
 				<span class="fa fa-lg fa-comment"></span>
-				<span class="fa fa-lg fa-paper-plane"></span>
 				<span class="fa fa-lg fa-user-circle"></span>
 				<span class="fa fa-lg fa-street-view"></span>
 				<span class="fa fa-lg fa-camera-retro"></span>
-				<span class="fa fa-lg fa-info-circle"></span>
 				<span class="fa fa-lg fa-times"></span>
 			</div>
 		</div>`);
+	//<span class="fa fa-lg fa-paper-plane"></span> 飞机
+	//<span class="fa fa-lg fa-info-circle"></span> 历史记录
 	// https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
 	setTimeout(() => {
 		document.getElementById("waifu").style.bottom = 0;
@@ -55,16 +55,17 @@ function loadWidget(config) {
 
 	(function registerEventListener() {
 		document.querySelector("#waifu-tool .fa-comment").addEventListener("click", showHitokoto);
-		document.querySelector("#waifu-tool .fa-paper-plane").addEventListener("click", () => {
-			if (window.Asteroids) {
-				if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
-				window.ASTEROIDSPLAYERS.push(new Asteroids());
-			} else {
-				let script = document.createElement("script");
-				script.src = "https://cdn.jsdelivr.net/gh/GalaxyMimi/CDN/asteroids.js";
-				document.head.appendChild(script);
-			}
-		});
+		// 飞机代码
+		// document.querySelector("#waifu-tool .fa-paper-plane").addEventListener("click", () => {
+		// 	if (window.Asteroids) {
+		// 		if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
+		// 		window.ASTEROIDSPLAYERS.push(new Asteroids());
+		// 	} else {
+		// 		let script = document.createElement("script");
+		// 		script.src = "https://cdn.jsdelivr.net/gh/GalaxyMimi/CDN/asteroids.js";
+		// 		document.head.appendChild(script);
+		// 	}
+		// });
 		document.querySelector("#waifu-tool .fa-user-circle").addEventListener("click", loadOtherModel);
 		document.querySelector("#waifu-tool .fa-street-view").addEventListener("click", loadRandModel);
 		document.querySelector("#waifu-tool .fa-camera-retro").addEventListener("click", () => {
@@ -73,7 +74,7 @@ function loadWidget(config) {
 			Live2D.captureFrame = true;
 		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
+			open("https://github.com/AiCyan/jscdn/tree/master/live2d-widget");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
